bc86b4f1855b6b5306a7178a0e0b4202a8520315
amit1-ctrl <amitmazharulislam@gmail.com>
2026-05-02 10:42:10 +0600

    Fixed email for Render

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
